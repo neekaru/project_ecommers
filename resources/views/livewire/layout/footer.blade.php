ea2b669095fa80de56c1eb3b75c96cbd34a1790d
estateresia <estateresiameilana04@gmail.com>
2025-05-27 15:07:13 +0700

    tambahan shadow

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -5,18 +5,15 @@
             <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 <h4>Bebek Galak Rara 57</h4>
                 <p>
-                    Bebek Galak Rara 57 adalah usaha kuliner
-                    milik Ibu Karsini yang berdiri sejak 2016.
-                    Berawal dari hobi memasak, usaha ini
-                    menyajikan menu andalan ayam dan bebek
-                    dengan cita rasa rumahan. Dengan pelayanan
-                    ramah dan rasa yang lezat, UMKM ini berkembang
+                    Bebek Galak Rara 57 adalah usaha kuliner milik Ibu Karsini yang berdiri sejak 2016.
+                    Berawal dari hobi memasak, usaha ini menyajikan menu andalan ayam dan bebek
+                    dengan cita rasa rumahan. Dengan pelayanan ramah dan rasa yang lezat, UMKM ini berkembang
                     cukup baik dan menjadi pilihan favorit pelanggan.
                 </p>
             </div>
 
-            <!-- Kolom Open Hours -->
-            <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Kolom Open Hours - Posisi Tengah -->
+            <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 text-center">
                 <h4>Open Hours</h4>
                 <ul class="list-unstyled open-hours-list">
                     <li><span>Senin</span> <span>10:00 - 21:00</span></li>
@@ -29,27 +26,28 @@
                 </ul>
             </div>
 
-            <!-- Kolom Alamat & Contact -->
-            <div class="col-lg-5 col-md-12 mb-4 mb-lg-0">
+            <!-- Kolom Alamat & Contact - Posisi Kanan -->
+            <div class="col-lg-5 col-md-12 mb-4 mb-lg-0 text-lg-start ps-lg-5">
                 <h4>Alamat</h4>
-                <p>
+                <p class="mb-0">
                     Jl. Wengga Jaya Agung Jalur 15 No.320<br>
                     Kalimantan Tengah, Kab. Kotawaringin<br>
                     Timur, Sampit<br>
                     74312
                 </p>
-                
+
                 <h4 class="mt-4">Contact</h4>
-                <div class="contact-item">
+                <div class="contact-item justify-content-lg-start">
                     <i class="bi bi-whatsapp"></i>
                     <span>0852-5279-3436</span>
                 </div>
-                <div class="contact-item">
+                <div class="contact-item justify-content-lg-start">
                     <i class="bi bi-instagram"></i>
                     <span>@bebekgalakrara57</span>
                 </div>
             </div>
         </div>
+
 
         <hr class="my-4" style="border-color: rgba(255,255,255,0.2);">
 
