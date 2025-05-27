35504f1d5fa11d552cbc15b74d5a26a10886eb9a
estateresia <estateresiameilana04@gmail.com>
2025-05-27 10:52:57 +0700

    tes namira bau kambing

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    // Kolom yang boleh diisi mass assignment
+    // Kolom yang boleh diisi mass assignment namiraaa bau kambing
     protected $fillable = [
         'image',
         'name',
